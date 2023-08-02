19591f6a4aa7f5d7758ff099b79eccadc5e1a4f4
Rhys Griffiths <rhys.griffiths@arm.com>
2023-08-02 19:48:17 +0100

    ETMv3 support

--- my_app/java_etmv4_c_api/build.sh
+++ my_app/java_etmv4_c_api/build.sh
@@ -15,7 +15,7 @@ mv Callback.h.keep Callback.h
 mv Callback2.h.keep Callback2.h
 
 mkdir -p com/arm/debug/trace/decode/jopencsd
-SWIG_LIB=../java/swig-4.1.1/Lib  ../java/swig-4.1.1/swig -java -package com.arm.debug.trace.decode.jopencsd -outdir com/arm/debug/trace/decode/jopencsd -c++ -I../../decoder/include -I../../decoder/include/opencsd -I../../decoder/include/opencsd/c_api -I../../decoder/include/common -I../../decoder/include/interfaces -I../../decoder/include/opencsd/etmv4 -I../../decoder/include/pkt_printers jopencsd.i
+SWIG_LIB=../java/swig-4.1.1/Lib  ../java/swig-4.1.1/swig -java -package com.arm.debug.trace.decode.jopencsd -outdir com/arm/debug/trace/decode/jopencsd -c++ -I../../decoder/include -I../../decoder/include/opencsd -I../../decoder/include/opencsd/c_api -I../../decoder/include/common -I../../decoder/include/interfaces -I../../decoder/include/opencsd/etmv4 -I../../decoder/include/opencsd/etmv3 -I../../decoder/include/pkt_printers jopencsd.i
 
 DS_JOPENCSD_DIR=/opt/ds/Dev_Arm_DS/work/ds/0.0/source/devstudio/plugins/debugger/com.arm.debug.trace/src/com/arm/debug/trace/decode/jopencsd
 if [ -d "$DS_JOPENCSD_DIR" ]; then
@@ -26,7 +26,7 @@ fi
 
 echo 1
 # TODO remove -fpermissive
-g++ -c -fpermissive -fpic -I../../decoder/include -I../../decoder/include/opencsd -I../../decoder/include/opencsd/c_api -I../../decoder/include/common -I../../decoder/include/interfaces -I../../decoder/include/opencsd/etmv4 -I../../decoder/include/pkt_printers -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" jopencsd_wrap.cxx
+g++ -c -fpermissive -fpic -I../../decoder/include -I../../decoder/include/opencsd -I../../decoder/include/opencsd/c_api -I../../decoder/include/common -I../../decoder/include/interfaces -I../../decoder/include/opencsd/etmv4 -I../../decoder/include/opencsd/etmv3 -I../../decoder/include/pkt_printers -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" jopencsd_wrap.cxx
 
 echo 2
 g++ -shared -Wl,-z,defs -Wl,-rpath=../../build/decoder/build/rctdl_c_api_lib -Wl,-rpath=../../build/decoder/build/ref_trace_decode_lib -L../../build/decoder/build/rctdl_c_api_lib -L../../build/decoder/build/ref_trace_decode_lib jopencsd_wrap.o -lopencsd_c_api -lopencsd -o libjopencsd.so
