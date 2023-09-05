9443099e0bac21271b1fbdc7593d57544c322c3f
Rhys Griffiths <rhys.griffiths@arm.com>
2023-09-05 17:03:20 +0100

    Windows support

--- cmake_simple_example/lib/logger.h
+++ cmake_simple_example/lib/logger.h
@@ -2,7 +2,21 @@
 
 #include <string>
 
-class Logger {
+
+#ifdef WIN32
+    #ifdef logger_EXPORTS
+        // We are building this library
+        #define LOGGER_EXPORT __declspec(dllexport)
+    #else
+        // We are using this library
+        #define LOGGER_EXPORT __declspec(dllimport)
+    #endif
+#else
+    // define as nothing
+    #define LOGGER_EXPORT
+#endif
+
+class LOGGER_EXPORT Logger {
 public:
     void logMsg(std::string msg);
 };
